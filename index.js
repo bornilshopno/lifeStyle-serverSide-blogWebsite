524a7d1ef3d0aefeb4852e2ce65e75f4ba8833db
Ashraf Manna <optimisticashraf@gmail.com>
2024-12-23 02:26:19 +0600

    will resume the balance work tomorrow

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app=express();
 const port= process.env.PORT || 5000
@@ -47,6 +47,13 @@ async function run() {
         const cursor=blogCollection.find();
         const result=await cursor.toArray();
         res.send(result);
+    })
+
+    app.get("/blog/:id", async (req,res)=>{
+        const id=req.params.id;
+        const query={_id: new ObjectId(id)}
+        const result= await blogCollection.findOne(query)
+        res.send(result)
     })
   
 
