4ec89707637a1a623d257ae9f90861427ead5b18
Ashraf Manna <optimisticashraf@gmail.com>
2024-12-25 23:58:42 +0600

    JWT setup remaining only

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt= require ('jsonwebtoken');
+const cookieParser=require('cookie-parser')
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
@@ -8,6 +10,7 @@ const port = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(cors())
+app.use(cookieParser())
 
 app.get("/", (req, res) => {
   res.send("Server Running")
